b8c165ca44390fa125ad002e7da3ce2784d6015b
yasin.gunes <yasin01ysn@gmail.com>
2023-05-03 18:16:39 +0300

    Initial commit

---
+++ lib/login_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              labelText: 'Email',
+              icon: Icon(Icons.email),
+            ),
+          ),
+          SizedBox(height: 16.0),
+          TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Password',
+              icon: Icon(Icons.lock),
+            ),
+          ),
+          SizedBox(height: 32.0),
+          ElevatedButton(
+            child: Text('Login'),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    ),
+  );
+  }
+}
